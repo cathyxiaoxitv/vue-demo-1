602301987ffca049c7dffa7f80db0fd3de625c70
cathyxiaoxitv <melodyxiaoxi@gmail.com>
2020-08-08 16:15:55 +0900

    update

---
+++ src/Demo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="red">
+    cathy's demo
+  </div>
+</template>
+
+<script>
+export default {
+  el:'#app',
+  data(){
+    return{
+      n:0
+    }
+  },
+  methods:{
+    add(){
+      this.n += 1
+    }
+  }
+}
+</script>
+
+<style scoped>
+.red{
+  color: red;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,45 @@
-import Vue from 'vue'
-import App from './App.vue'
 
+const Vue = window.Vue
+console.log(Vue);
 Vue.config.productionTip = false
 
+import Demo from './Demo.vue'
+
+console.log(`here`);
+Vue.component('demo2',{
+    template:`
+    <div>11112</div>
+    `
+})
+console.log(`demo`);
+console.log(Demo);
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    components:{
+        Frank: Demo
+    },
+    data() {
+        return {
+            n: 0,
+            array: [1, 2, 3, 4, 5, 6, 7, 8]
+        }
+    },
+    template: `
+      <div class="red">
+      {{n}}
+      <button @click="add">+1</button>
+      <Frank/>
+      <hr>
+      {{ filter()}}   
+      </div>`,
+    methods: {
+        add() {
+            this.n += 1
+        },
+        filter() {
+            console.log(`did filter`);
+            return this.array.filter(i => i % 2 === 0)
+        }
+    }
+
+})
